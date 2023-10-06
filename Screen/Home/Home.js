02e317f560f37eab95e009317c1d7666849f39ce
thanhtamqn29 <144640484+thanhtamqn29@users.noreply.github.com>
2023-10-06 14:57:08 +0700

    Feat/dpm homepage (#5)

--- Screen/Home/Home.js
+++ Screen/Home/Home.js
@@ -14,7 +14,10 @@ import { Ionicons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 
+import { useNavigation } from "@react-navigation/native";
 const Home = () => {
+  const navigation = useNavigation();
+
   const listRestaurant = [
     {
       id: "0",
@@ -216,7 +219,9 @@ const Home = () => {
             <Text style={{ fontWeight: "700", fontSize: 17 }}>
               Nearest Restaurant
             </Text>
-            <Pressable>
+
+            <Pressable  onPress={() => navigation.navigate("Detail")}>
+
               <Text style={{ fontSize: 12, color: "#6B50F6" }}>View More</Text>
             </Pressable>
           </View>
