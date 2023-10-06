42ecd666b8ff57c612bbc75be2d6724a48dacb57
tam <144640484+thanhtamqn29@users.noreply.github.com>
2023-10-06 13:31:13 +0700

    done

---
+++ Component/RestaurantCart/RestaurantCart.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RestaurantCart = () => {
+  return (
+    <View>
+      <Text>RestaurantCart</Text>
+    </View>
+  )
+}
+
+export default RestaurantCart
+
+const styles = StyleSheet.create({})
