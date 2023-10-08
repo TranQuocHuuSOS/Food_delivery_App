bb5e91354c12bf1dd00671275051439db2567434
Trần Quốc Hữu <134064997+TranQuocHuuSOS@users.noreply.github.com>
2023-10-08 21:50:20 +0700

    Delete order (#11)
    * process delete Order
    
    * push deleteOrder again

--- Screen/Cart/Cart.js
+++ Screen/Cart/Cart.js
@@ -17,8 +17,10 @@ import { AntDesign } from "@expo/vector-icons";
 const image = require("../../assets/Pattern.png");
 const image_total = require("../../assets/Order_detail/Pattern.png");
 import { SwipeListView } from "react-native-swipe-list-view";
+import Card_price from "../Card_total_price/Card_price";
 const Cart = () => {
   const [quantity, setQuantity] = useState({});
+  const [selectedItems, setSelectedItems] = useState([]);
   let totalPrice = 0;
   const data = [
     {
@@ -27,7 +29,7 @@ const Cart = () => {
       name_restaurant: "kita",
       price: 35,
       menuImage: require("../../assets/MenuPhoto.png"),
-      discount:10,
+      discount: 10,
     },
     {
       key: "2",
@@ -35,7 +37,7 @@ const Cart = () => {
       name_restaurant: "kita",
       price: 35,
       menuImage: require("../../assets/MenuPhoto1.png"),
-      discount:10,
+      discount: 10,
     },
     {
       key: "3",
@@ -43,7 +45,7 @@ const Cart = () => {
       name_restaurant: "kita",
       price: 35,
       menuImage: require("../../assets/MenuPhoto2.png"),
-      discount:10,
+      discount: 10,
     },
     {
       key: "4",
@@ -51,7 +53,7 @@ const Cart = () => {
       name_restaurant: "kita",
       price: 35,
       menuImage: require("../../assets/MenuPhoto.png"),
-      discount:10,
+      discount: 10,
     },
     {
       key: "5",
@@ -59,7 +61,7 @@ const Cart = () => {
       name_restaurant: "kita",
       price: 35,
       menuImage: require("../../assets/MenuPhoto1.png"),
-      discount:10,
+      discount: 10,
     },
   ];
 
@@ -86,54 +88,73 @@ const Cart = () => {
       totalPrice += itemPrice;
     });
 
-    
-    totalPrice += 10; 
+    totalPrice += 10;
 
-    return totalPrice.toFixed(2); 
+    return totalPrice.toFixed(2);
   };
-  const renderRow = (key, name_dish, name_restaurant, price, menuImage, discount) => (
-    <TouchableOpacity
-      onPress={() => console.log(`Item ${key} touched`)}
-      style={styles.itemContainer}
-      key={key}
-    >
-      <Text style={styles.itemText}>
-        <View style={styles.card}>
-          <Image source={menuImage} style={{ width: 70, height: 70 }}></Image>
-          <View style={{ marginHorizontal: 10 }}>
-            <Text style={{ fontWeight: "bold" }}>{name_dish}</Text>
-            <Text style={{ color: "#000" }}>{name_restaurant}</Text>
-            <Text
-              style={{ fontWeight: "bold", color: "#6B50F6", fontSize: 20 }}
-            >
-              $ {price}
-            </Text>
+  const renderRow = (
+    key,
+    name_dish,
+    name_restaurant,
+    price,
+    menuImage,
+    discount
+  ) => {
+    const isSelected = selectedItems.includes(key);
+    if (isSelected) {
+      return null;
+    }
+    return (
+      <TouchableOpacity
+        onPress={() => console.log(`Item ${key} touched`)}
+        style={styles.itemContainer}
+        key={key}
+      >
+        <Text style={styles.itemText}>
+          <View style={styles.card}>
+            <Image source={menuImage} style={{ width: 70, height: 70 }}></Image>
+            <View style={{ marginHorizontal: 10 }}>
+              <Text style={{ fontWeight: "bold" }}>{name_dish}</Text>
+              <Text style={{ color: "#000" }}>{name_restaurant}</Text>
+              <Text
+                style={{ fontWeight: "bold", color: "#6B50F6", fontSize: 20 }}
+              >
+                $ {price}
+              </Text>
+            </View>
+            <View style={styles.amount}>
+              <TouchableOpacity
+                style={styles.plus}
+                onPress={() => decreaseQuantity(key)}
+              >
+                <AntDesign name="minus" size={20} color="black" />
+              </TouchableOpacity>
+              <Text style={styles.number}>{quantity[key] || 1}</Text>
+              <TouchableOpacity
+                style={styles.minus}
+                onPress={() => increaseQuantity(key)}
+              >
+                <AntDesign name="plus" size={20} color="white" />
+              </TouchableOpacity>
+            </View>
           </View>
-          <View style={styles.amount}>
-            <TouchableOpacity
-              style={styles.plus}
-              onPress={() => decreaseQuantity(key)}
-            >
-              <AntDesign name="minus" size={20} color="black" />
-            </TouchableOpacity>
-            <Text style={styles.number}>{quantity[key] || 1}</Text>
-            <TouchableOpacity
-              style={styles.minus}
-              onPress={() => increaseQuantity(key)}
-            >
-              <AntDesign name="plus" size={20} color="white" />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Text>
-    </TouchableOpacity>
-  );
-
-  const renderHiddenItem = () => (
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+  const handleDeleteItem = (key) => {
+    console.log("Delete item with key:", key);
+    setSelectedItems((prevSelectedItems) =>
+      prevSelectedItems.includes(key)
+        ? prevSelectedItems.filter((itemKey) => itemKey !== key)
+        : [...prevSelectedItems, key]
+    );
+  };
+  const renderHiddenItem = (data) => (
     <View style={styles.hiddenContainer}>
       <TouchableOpacity
         style={[styles.hiddenButton, styles.deleteButton]}
-        onPress={() => console.log("Delete Button pressed")}
+        onPress={() => handleDeleteItem(data.item.key)}
       >
         <AntDesign name="delete" size={28} color="white" />
       </TouchableOpacity>
@@ -173,42 +194,13 @@ const Cart = () => {
             previewOpenDelay={3000}
           />
         </View>
-        <View style={styles.container_total}>
-          <ImageBackground
-            source={image_total}
-            resizeMode="cover"
-            style={styles.imageTotal}
-          >
-            <View style={{ marginTop: 20, marginHorizontal: 20 }}>
-              <View style={styles.sub_total}>
-                <Text style={styles.text_total}>Sub_total</Text>
-                <Text style={styles.number_total}> ${data.reduce(
-                    (acc, item) =>
-                      acc +
-                      (item.price - item.discount) * (quantity[item.key] || 1),
-                    0
-                  ).toFixed(2)}</Text>
-              </View>
-              <View style={styles.delivery}>
-                <Text style={styles.text_total}>Delivery Charge</Text>
-                <Text style={styles.number_total}>10 $</Text>
-              </View>
-              <View style={styles.discount}>
-                <Text style={styles.text_total}>Discount</Text>
-                <Text style={styles.number_total}>10$</Text>
-              </View>
-            </View>
-            <View style={styles.total}>
-              <Text style={styles.text_totals}>Total</Text>
-              <Text style={styles.number_totals}>${calculateTotalPrice()}</Text>
-            </View>
-            <TouchableOpacity>
-              <View style={styles.button}>
-                <Text style={styles.text_button}>Place My Order</Text>
-              </View>
-            </TouchableOpacity>
-          </ImageBackground>
-        </View>
+       
+        <Card_price
+          data={data}
+          selectedItems={selectedItems}
+          quantity={quantity}
+          calculateTotalPrice={calculateTotalPrice}
+        />
       </ScrollView>
     </SafeAreaView>
   );
---
+++ Screen/Payment/Payment.js
@@ -0,0 +1,23 @@
+import { ImageBackground,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    Pressable,
+    TextInput,
+    Button,
+    TouchableOpacity,
+    Image, } from 'react-native'
+import React from 'react'
+
+const Payment = () => {
+  return (
+    <View>
+      <Text>Payment</Text>
+    </View>
+  )
+}
+
+export default Payment
+
+const styles = StyleSheet.create({})
